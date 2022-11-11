0b27df115e449de276bb0387d4f93016964b5599
Aleksander Skula <aleksander.skula@gmail.com>
2022-11-11 14:21:54 +0100

    adding new tasks function

--- js/script.js
+++ js/script.js
@@ -14,15 +14,40 @@
     const render = () => {
         let listItem = "";
 
-        for(const task of tasks) {
-            listItem +=`<li>${task.content}</li>`;
+        for (const task of tasks) {
+            listItem += `<li
+            class=${task.finished === true ? "taskFinished" : ""}>
+            ${task.content}
+            </li>`;
         };
 
-      document.querySelector(".js-tasksList").innerHTML = listItem;  
+        document.querySelector(".js-tasksList").innerHTML = listItem;
     };
 
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", event => {
+        event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    console.log(newTaskContent);
+
+    if (newTaskContent === "") {
+        return;
+    } 
+    tasks.push({
+        content: newTaskContent,
+    });
+
+    render();
+
+    });
+
+
     };
 
     init();
