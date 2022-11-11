192f1ce55a702dde141b71d182a3bc97eb7412fc
Aleksander Skula <aleksander.skula@gmail.com>
2022-11-11 15:10:58 +0100

    finish and delete buttons added

--- js/script.js
+++ js/script.js
@@ -17,12 +17,40 @@
         for (const task of tasks) {
             listItem += `<li
             class=${task.finished === true ? "taskFinished" : ""}>
+            <button class="js-finishButtons">Zrobione</button>
             ${task.content}
+            <button class="js-deleteButtons">Usuń zadanie</button>
             </li>`;
         };
 
         document.querySelector(".js-tasksList").innerHTML = listItem;
+
+        const deleteButtons = document.querySelectorAll(".js-deleteButtons");
+
+        deleteButtons.forEach((deleteButton, index) => {
+            deleteButton.addEventListener("click", () => {
+                deleteTask(index);
+            });
+        });
+
+        const finishButtons = document.querySelectorAll(".js-finishButtons");
+
+        finishButtons.forEach((finishButton, index) => {
+            finishButton.addEventListener("click", () => {
+                finishTask(index);
+            });
+        });
     };
+
+    const deleteTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    }
+
+    const finishTask = (index) => {
+        tasks[index].finished = !tasks[index].finished;
+        render();
+    }
 
     const addNewTask = (newTaskContent) => {
 
